58807f9a0593816fe9f62fcf8dc3c145519f0f17
mehran <mahmoudi1199@gmail.com>
2018-07-01 15:49:39 +0430

    libs fat jar

--- src/main/java/com/mehran/main/Main.java
+++ src/main/java/com/mehran/main/Main.java
@@ -1,7 +1,8 @@
 package com.mehran.main;
-
+import edu.stanford.nlp.tmt.*;
 public class Main {
     public static void main(String args[]){
+        TMTMain.main(args);
         System.out.print("sam");
     }
 }
